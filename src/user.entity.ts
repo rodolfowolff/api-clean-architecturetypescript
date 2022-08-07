6e329ecd71fdddcf71e1f4b996a70e534093d77d
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-08-07 11:38:50 -0300

    feat: metodo update user info

--- src/user.entity.ts
+++ src/user.entity.ts
@@ -18,6 +18,12 @@ export type UserType = {
   deleted_at: Date | null;
 };
 
+export type UserInfo = {
+  firstname?: string;
+  lastname?: string;
+  telephone?: string;
+};
+
 export class User {
   public props: Required<UserType>; // Required = Todos os campos obrigatórios
   // CONSTRUTOR GERA UM OBJETO USUARIO
@@ -27,5 +33,31 @@ export class User {
       daysService: props.daysService || [], // Se não tiver daysService, atribui um array vazio
       deleted_at: props.deleted_at || null, // Se não tiver deleted_at, atribui null
     };
+  }
+
+  // METODOs PARA ATUALIZAR OS DADOS DO USUARIO
+  updateUserInfo(userinfo: UserInfo) {
+    this.props = {
+      ...this.props,
+      firstname: userinfo.firstname || this.props.firstname,
+      lastname: userinfo.lastname || this.props.lastname,
+      telephone: userinfo.telephone || this.props.telephone,
+    };
+  }
+
+  // PUBLIC = PODE SER ACESSADO FORA DA CLASSE
+  get userinfo() {
+    return {
+      firstname: this.props.firstname,
+      lastname: this.props.lastname,
+      telephone: this.props.telephone,
+    };
+  }
+
+  // PRIVADO = NÃO PODE SER ACESSADO FORA DA CLASSE
+  private set userinfo(userinfo: UserInfo) {
+    this.props.firstname = userinfo.firstname || this.props.firstname;
+    this.props.lastname = userinfo.lastname || this.props.lastname;
+    this.props.telephone = userinfo.telephone || this.props.telephone;
   }
 }
