fd2d12caf743766a6e1e94b85016cfe41bd4be0a
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-08-07 11:38:56 -0300

    test: metodo update user info

--- src/user.entity.spec.ts
+++ src/user.entity.spec.ts
@@ -45,4 +45,51 @@ describe("User Tests", () => {
       deleted_at: null,
     });
   });
+
+  it("update user info", () => {
+    let userProps: UserType = {
+      id: 1,
+      firstname: "John",
+      lastname: "Doe",
+      telephone: "99999999",
+      password: "123456",
+      sector: [{ id: 1, name: "Sector 1" }],
+      status: true,
+      created_at: new Date(),
+      updated_at: new Date(),
+      deleted_at: null,
+    };
+
+    let user = new User(userProps);
+
+    let updatename = {
+      firstname: "Jane",
+      lastname: "Adams",
+    };
+
+    let updateuser = user.updateUserInfo(updatename);
+
+    expect(user.props).toStrictEqual({
+      ...userProps,
+      firstname: "Jane",
+      lastname: "Adams",
+      daysService: [],
+      deleted_at: null,
+    });
+
+    let updateTelephone = {
+      telephone: "88888888",
+    };
+
+    updateuser = user.updateUserInfo(updateTelephone);
+
+    expect(user.props).toStrictEqual({
+      ...userProps,
+      firstname: "Jane",
+      lastname: "Adams",
+      telephone: "88888888",
+      daysService: [],
+      deleted_at: null,
+    });
+  });
 });
